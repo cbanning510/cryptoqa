60b3a7629254bfeedee75553a238267a5a335f9c
Chris Banning <chrisbanning510@gmail.com>
2024-05-03 23:25:06 -0400

    add https

--- ChatComponent.js
+++ ChatComponent.js
@@ -184,7 +184,7 @@ const ChatComponent = () => {
 
         try {
           const response = await fetch(
-            "http://ec2-44-202-254-222.compute-1.amazonaws.com:3001/openai/message",
+            "https://ec2-44-202-254-222.compute-1.amazonaws.com:3001/openai/message",
             // "http://192.168.1.151:3001/openai/message",
             {
               method: "POST",
